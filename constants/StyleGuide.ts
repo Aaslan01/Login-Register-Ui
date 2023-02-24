0308033be8272501d598b86c3ef09a8fae22d654
Ali Sher Khan <72862093+Aaslan01@users.noreply.github.com>
2023-02-24 15:15:50 +0500

    Open Screen under-construction

---
+++ constants/StyleGuide.ts
@@ -0,0 +1,23 @@
+const white = '#fff';
+const black = '#000';
+const dark = '#626262';
+const blue = '#1F41BB';
+const gray = '#ECECEC';
+const lightBlue = '#f1f4ff';
+
+const StyleGuide = {
+  colors: {
+    darkText: dark,
+    text: black,
+    background: white,
+    primary: blue,
+    onPrimary: white,
+    active: blue,
+    borderWithOpacity: '#1f41bb',
+    lightPrimary: lightBlue,
+    gray: gray,
+  },
+  
+};
+
+export default StyleGuide;
--- screen/OpeningScreen.tsx
+++ screen/OpeningScreen.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Image, SafeAreaView} from 'react-native';
+import React from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import StyleGuide from '../constants/StyleGuide';
+import FontSize from '../constants/FontSize';
 
 const OpeningScreen = () => {
   return (
-    <View>
-      <Text style={{color: "black"}}>OpeningScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{
+        backgroundColor: StyleGuide.colors.background,
+        flex: 1,
+        padding: wp(3),
+      }}>
+      <Image
+        style={{
+          margin: wp(4),
+          marginTop: hp(6),
+          height: hp(40),
+          width: wp(80),
+        }}
+        source={require('../assets/images/welcome-img.png')}
+      />
+      <Text
+        style={{
+          fontFamily: 'Poppins-Bold',
+          fontSize: FontSize.xLarge,
+          color: StyleGuide.colors.primary,
+          paddingHorizontal: wp(10),
+          textAlign: 'center',
+        }}>
+        Discover You Dream Job Here
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'Poppins-Regular',
+          fontSize: FontSize.small,
+          color: StyleGuide.colors.text,
+          paddingHorizontal: wp(10),
+          textAlign: 'center',
+        }}>
+        Explore the all exiting job roles based on your interest and study major
+      </Text>
+    </SafeAreaView>
+  );
+};
 
-export default OpeningScreen
+export default OpeningScreen;
